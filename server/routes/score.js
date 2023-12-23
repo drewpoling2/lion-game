021b4d5cb62baf1e60b93dc2ed6f6b089e42b108
Drew Poling <88001099+drewpoling2@users.noreply.github.com>
2023-12-22 22:54:50 -0500

    rm ts from server

---
+++ server/routes/score.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+const router = Router();
+import {
+  createNewHigh,
+  getAllUsers,
+  deleteEntryByUsername,
+} from '../controllers /score.js';
+
+router.delete('/delete-entry/:username', deleteEntryByUsername);
+router.post('/new-high', createNewHigh);
+router.get('/users', getAllUsers);
+
+export { router as scoreRouter };
